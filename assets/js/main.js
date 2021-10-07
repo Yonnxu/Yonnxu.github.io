4b7c5d842f0c829e79b4d76ef6634b5eadf6f0f6
Administrator <2195249372@qq.com>
2021-10-07 10:12:17 +0800

    '更新'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,29 @@
 // Little people
-function f(){
+function f() {
     L2Dwidget.init({
         model: {jsonPath: "live2d/live2dw/assets/asuna_04.model.json"}
     })
-
 }
-f()
 
+f();
+
+// Dom加载完成，移除Loading
+
+$(function(){
+    $(".loading").remove()
+});
+    // 监听加载状态改变
+    // document.onreadystatechange = completeLoading;
+    // console.log(document.readyState)
+    // // 加载状态为complete时移除loading效果
+    // function completeLoading() {
+    //     if (document.readyState === "complete") {
+    //         var loadingMask = document.getElementById('svg');
+    //         loadingMask.parentNode.removeChild(loadingMask);
+    //         console.log(document.readyState)
+    //     }
+    // }
+// }
 // Dynamic Title
 window.onfocus = function () {
     document.title = 'Administratorの博客 ~(@^_^@)~...';
