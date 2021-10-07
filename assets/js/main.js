819e7ddcefb46a63f2d33645c49a88d3156df6db
Administrator <2195249372@qq.com>
2021-10-07 11:41:29 +0800

    '修改名称'

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,23 +10,12 @@ f();
 // Dom加载完成，移除Loading
 
 $(function(){
-    $(".loading").remove()
+    $(".loadings").remove()
 });
-    // 监听加载状态改变
-    // document.onreadystatechange = completeLoading;
-    // console.log(document.readyState)
-    // // 加载状态为complete时移除loading效果
-    // function completeLoading() {
-    //     if (document.readyState === "complete") {
-    //         var loadingMask = document.getElementById('svg');
-    //         loadingMask.parentNode.removeChild(loadingMask);
-    //         console.log(document.readyState)
-    //     }
-    // }
-// }
+
 // Dynamic Title
 window.onfocus = function () {
-    document.title = 'Administratorの博客 ~(@^_^@)~...';
+    document.title = 'Administrator';
 };
 window.onblur = function () {
     document.title = '快回来~页面崩溃了 ┭┮﹏┭┮';
