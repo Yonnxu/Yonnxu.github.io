9b7f1b91ec427d6598472775dcfd1269ae2bc192
Administrator <2195249372@qq.com>
2021-10-10 00:13:59 +0800

    '滚动文字'

---
+++ assets/js/textStyle/index.js
@@ -0,0 +1,13 @@
+new Typed(".element",{
+    strings:[
+        "Time is money.",
+        "欠下的债，总是要还的。",
+        "现在一时的快活，成就将来后悔的自己。",
+        "出卖灵魂并不丢人，丢人的是没能卖一个好价钱。",
+        "我们所经历的每个平凡的日常，也许就是连续发生的奇迹。"
+    ],
+    typeSpeed:175,
+    loopCount: Infinity,
+    loop:true,
+    smartBackspace:true
+})
