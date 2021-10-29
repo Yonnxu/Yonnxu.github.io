9dfddd134c1352ec99c3d64f99b2e9f62a6d648a
Administrator <2195249372@qq.com>
2021-10-29 10:52:22 +0800

    '增加加载页面的淡出效果'

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,9 @@ f();
 // Dom加载完成，移除Loading
 
 $(function(){
-    $(".loadings").remove();
+    $(".loadings").fadeTo("slow", 0.2, function(){
+            $(".loadings").remove();
+    });
     $(".gtr-25 div").hover(function () {
         $(this).siblings().stop().fadeTo(1000,0.5)
     },function () {
